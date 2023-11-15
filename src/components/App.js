a7afb727feb031af1966725d38a8fe19d36b30d6
ian-bravo <bravo.ian@gmail.com>
2023-11-14 16:06:06 -0800

    add CSS
    Co-authored-by: Joey Palchak <joey.palchak@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 // import Header from "./Header";
-// import MonthControl from "./MonthControl";
+import MonthControl from "./MonthControl";
 import DayControl from "./DayControl";
+import "./../App.css";
 
 function App(){
   return ( 
     <React.Fragment>
-      {/* <Header />
-      <MonthControl /> */}
+      {/* <Header /> */}
+      <MonthControl />
       <DayControl />
     </React.Fragment>
   );
